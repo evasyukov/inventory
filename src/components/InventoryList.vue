74e3c76e4f79ce40437489dd6d2e042d5b976219
evasyukov <egor.vasyukov1@gmail.com>
2024-07-27 10:03:04 +0700

    Добавлена карточка предмета

--- src/components/InventoryList.vue
+++ src/components/InventoryList.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="inventory-list">
-    
+    <div class="inventory-list_cell" v-for="n in 25" :key="n">
+      <Item v-if="n < 4" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import Item from "./Item.vue"
+
 export default {
   name: "InventoryList",
+
+  components: {
+    Item,
+  },
 }
 </script>
 
 <style lang="scss">
 .inventory-list {
-  width: 1080px;
+  width: 1090px;
   height: 100%;
+
+  display: flex;
+  flex-wrap: wrap;
 
   background: #262626;
   border: 1px solid #4d4d4d;
   border-radius: 20px;
+
+  &_cell {
+    width: 200px;
+    height: 147px;
+
+    border: 1px solid #4d4d4d;
+
+    flex: 1 0 auto;
+  }
 }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="item" @click="openModal()">
+    <div class="item_first"><div class="item_second"></div></div>
+  </div>
+  <ModalItem :isVisible="showModal" @close="closeModal" />
+</template>
+
+<script lang="ts">
+import ModalItem from "./ModalItem.vue"
+
+export default {
+  name: "Item",
+  components: {
+    ModalItem,
+  },
+  data() {
+    return {
+      showModal: true,
+    }
+  },
+  methods: {
+    openModal() {
+      this.showModal = true
+    },
+    closeModal() {
+      this.showModal = false
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.item {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  &_first {
+    width: 70px;
+    height: 70px;
+
+    background-color: rgba(170, 151, 101, 1);
+  }
+
+  &_second {
+    width: 70px;
+    height: 70px;
+
+    position: relative;
+    bottom: 10px;
+    left: 10px;
+
+    z-index: 1;
+
+    background-color: rgba(217, 187, 152, 0.35);
+    backdrop-filter: blur(12px);
+  }
+}
+</style>
