e032c95e15637984d141add30413fadec0b76d64
evasyukov <egor.vasyukov1@gmail.com>
2024-10-23 12:57:14 +0700

    - задал общие стили - верстка главной страницы

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="inventory-menu">
+
+    <!-- Основной контент страницы -->
+    <div class="inventory-menu_main">
+
+      <!-- Блок "персонажа" -->
+
+      <!-- Инвентарь -->
+    </div>
+
+    <!-- Нижняя панель -->
+    <div class="inventory-menu_bottom"></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.inventory-menu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 950px;
+}
+</style>
