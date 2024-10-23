c5e33e5d3d7e136b36e2171954be877c667c153c
evasyukov <egor.vasyukov1@gmail.com>
2024-10-23 13:56:34 +0700

    настроил стили для ячеек

--- src/components/InventoryList.vue
+++ src/components/InventoryList.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="inventory-list">
-
     <!-- ячейка предмета -->
-    <div class="inventory-list_cell">
-
-    </div>
-
+    <div v-for="item in 25" :key="item" class="inventory-list_cell"></div>
   </div>
 </template>
 
@@ -18,11 +14,29 @@
 
   display: flex;
   flex-wrap: wrap;
-
+  justify-content: center;
+  gap: 0px;
+  
   position: relative;
-
-  background: #262626;
-  border: 1px solid #4d4d4d;
   border-radius: 20px;
+
+  &_cell {
+    width: 210px;
+    background: #262626;
+    border: 1px solid #4d4d4d;
+  }
+
+  &_cell:nth-child(1) {
+    border-radius: 20px 0 0;
+  }
+  &_cell:nth-child(5) {
+    border-radius: 0 20px 0 0;
+  }
+  &_cell:nth-child(21) {
+    border-radius: 0 0 0 20px;
+  }
+  &_cell:nth-child(25) {
+    border-radius: 0 0 20px 0;
+  }
 }
 </style>
