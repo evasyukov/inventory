7acc6bd63eca30c628759eab2aeba219b5140911
evasyukov <egor.vasyukov1@gmail.com>
2024-10-23 12:43:52 +0700

    добавление локального хранилища предметов

---
+++ src/store/storeItems.js
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia"
+
+export const useStoreItems = defineStore("storeItems", {
+  state: () => ({
+    items: [
+      {
+        id: 1,
+        counter: 4,
+        color_first: "#AA9765",
+        color_second: "rgba(217, 187, 152, 0.35)",
+      },
+      {
+        id: 2,
+        counter: 2,
+        color_first: "#7FAA65",
+        color_second: "rgb(184 217 152 / 35%)",
+      },
+      {
+        id: 3,
+        counter: 5,
+        color_first: "#656CAA",
+        color_second: "rgb(116 129 237 / 35%)",
+      },
+      {
+        id: 4,
+        counter: 9,
+        color_first: "#AA6965",
+        color_second: "rgb(149 72 72 / 33%)",
+      },
+      {
+        id: 5,
+        counter: 7,
+        color_first: "#656CAA",
+        color_second: "rgb(116 129 237 / 35%)",
+      },
+      {
+        id: 6,
+        counter: 15,
+        color_first: "#659EAA",
+        color_second: "rgb(72 135 149 / 34%)",
+      },
+    ],
+  }),
+})
